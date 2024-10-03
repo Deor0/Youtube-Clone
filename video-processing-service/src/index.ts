c8cc84c1e6b7aa092d9b3c038cb167f38c0a8486
de0r <oshanhamal3@gmail.com>
2024-10-03 09:49:23 -0500

    Update to index.ts to match the changes made to storage.ts

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -2,7 +2,14 @@ import express from "express";
 import { promisify } from "util";
 import fs from "fs";
 
-import { convertVideos, deleteProcessedVideo, deleteRawVideo, deleteVideo, downloadRawVideo, setUpDirectories, uploadProcessedVidoes } from "./storage";
+import {
+  convertVideos,
+  deleteProcessedVideo,
+  deleteRawVideo,
+  downloadRawVideo,
+  setUpDirectories,
+  uploadProcessedVidoes,
+} from "./storage";
 
 setUpDirectories();
 const app = express();
@@ -11,19 +18,20 @@ app.use(express.json());
 const stat = promisify(fs.stat);
 
 app.post("/process-video", async (req, res) => {
-  
   // Get the name from pub sub from gsc
   let data;
 
   try {
-    const message = Buffer.from(req.body.message.data, "base64").toString('utf8');
+    const message = Buffer.from(req.body.message.data, "base64").toString(
+      "utf8"
+    );
     data = JSON.parse(message);
-    if(!data.name) {
+    if (!data.name) {
       throw new Error("Invalid message payload recieved.");
-      }
-    } catch(err) {
-      console.error(err);
-      return res.status(400).send("Bad request: File name missing.")
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send("Bad request: File name missing.");
   }
 
   const inputFileName = data.name;
@@ -31,26 +39,25 @@ app.post("/process-video", async (req, res) => {
 
   await downloadRawVideo(inputFileName);
 
-  // Convert the file resolution 
+  // Convert the file resolution
   try {
     await convertVideos(inputFileName, outPutFileName);
   } catch (error) {
     await Promise.all([
       deleteRawVideo(inputFileName),
-      deleteProcessedVideo(outPutFileName)
-    ])
-    return res.status(500).send('Processing failed');
+      deleteProcessedVideo(outPutFileName),
+    ]);
+    return res.status(500).send("Processing failed");
   }
 
-
-  // Upload the processsed file to the Cloud Storage 
+  // Upload the processsed file to the Cloud Storage
   await uploadProcessedVidoes(outPutFileName);
   await Promise.all([
     deleteRawVideo(inputFileName),
-    deleteProcessedVideo(outPutFileName)
+    deleteProcessedVideo(outPutFileName),
   ]);
 
-  return res.status(200).send('Processing finished successfully');
+  return res.status(200).send("Processing finished successfully");
 });
 
 const port = process.env.PORT || 3000;
